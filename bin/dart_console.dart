7a4b23db0589263682f323fe34be9b7dbbb828c0
ViVaKR <admin@vivabj.com>
2023-04-07 11:47:24 +0900

    Add dart_console

--- bin/dart_console.dart
+++ bin/dart_console.dart
@@ -1,5 +1,11 @@
 import 'package:dart_console/dart_console.dart' as dart_console;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_console.calculate()}!');
+  String sNum = "12345";
+  int iNum = int.parse(sNum);
+  sNum = iNum.toString();
+  double dNum1 = double.parse(sNum);
+  double? dNum2 = double.tryParse("1.2a");
+
+  print('parse: $dNum1, tryParse: $dNum2');
 }
