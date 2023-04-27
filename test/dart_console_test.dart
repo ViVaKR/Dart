0265fb8fe906428cf889e15ac90cd99cb59baae7
ViVaKR <admin@vivabj.com>
2023-04-27 22:22:19 +0900

    Update dart test

--- test/dart_console_test.dart
+++ test/dart_console_test.dart
@@ -2,7 +2,7 @@ import 'package:dart_console/dart_console.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
+  test('The calculate function returns 42', () {
     expect(calculate(), 42);
   });
 }
